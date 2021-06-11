6bb33884b8c3fb054a3ca25cc585947898e5e296
KubEF <fiska2003@mail.ru>
2021-06-11 22:02:49 +0300

    commit playing

--- scripts-w/main.js
+++ scripts-w/main.js
@@ -1,25 +1,60 @@
-var myImg = document.querySelector('img');
-myImg.onclick = function() {
-    var mySrc = myImg.getAttribute('src');
-    if (mySrc === 'image/krug.png') {
-        myImg.setAttribute('src', 'image/treug.png');
+var randomNumber = Math.floor(Math.random() * 15) + 1;
+var question = document.querySelector('.number');
+var inputAns = document.querySelector('.inp');
+var submit = document.querySelector(".submit");
+var version = document.querySelector('.version');
+var guessCount = 1;
+var resetButton;
+question.textContent = String(2**randomNumber);
+function checkAns() {
+    var ans = Number(inputAns.value);
+    if (guessCount === 3){
+        version.textContent ="Хахахаха! Ты проиграл!";
+        version.style.backgroundColor = 'red';
+        GameOver();
     }else{
-        myImg.setAttribute('src', 'image/krug.png');
+        if (ans === randomNumber) {
+            version.textContent ="Примите мои поздравления, кем бы вы ни были, вы выиграли";
+            version.style.backgroundColor = 'green';
+            GameOver();
+        }else if (ans !== randomNumber){
+            version.textContent = 'Неверно, 2 в степени ' + String(ans) + ' это ' + String(2 ** ans);
+        }
     }
+    guessCount += 1;
+    inputAns.value= '';
+    inputAns.focus();
 }
-var myButton = document.querySelector('button');
-var myHeanding = document.querySelector('h1');
-function setUserName() {
-    var myName = prompt('Имя сваё введи');
-    localStorage.setItem('name', myName);
-    myHeanding.textContent = 'Ну привет, ' + myName;
+
+function GameOver () {
+    submit.disabled = true;
+    inputAns.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Сыграть снова';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+    inputAns.focus();
 }
-if (!localStorage.getItem('name')) {
-    setUserName()
-}else {
-    var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Ну привет, ' + storedName;
+
+function resetGame() {
+    guessCount = 0;
+    submit.disabled = false;
+    inputAns.disabled = false;
+    version.textContent = '';
+    version.style.backgroundColor = 'orange';
+    resetButton.parentNode.removeChild(resetButton);
+    randomNumber = Math.floor(Math.random() * 15) + 1;
+    question.textContent = String(2**randomNumber);
 }
-myButton.onclick = function() {
-    setUserName();
-}
+submit.addEventListener("click", checkAns);
+
+document.addEventListener("keyup", function(a){
+
+    if (a.key === "Enter"){
+        if (submit.disabled === true){
+            resetGame();
+        }else{
+        checkAns();
+        }
+    }
+});
